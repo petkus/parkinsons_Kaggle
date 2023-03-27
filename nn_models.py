886e6a0d3b4864643cfe6998a6fe1d7585a5a399
Peter Gylys-Colwell <petergc@gchouse.org>
2023-03-27 14:16:45 -0700

    some nn additions

--- nn_models.py
+++ nn_models.py
@@ -24,6 +24,26 @@ class DefogDataset(Dataset):
         df = pd.read_csv(id_path)
 
         X = torch.tensor(df[['AccV', 'AccML', 'AccAP']].values)
-        y = torch.tensor(df[['StartHesitation']].values)
+        y = torch.tensor(df[['StartHesitation', 'Turn', 'Walking']].values)
 
         return X, y
+    
+
+class RNN(torch.nn.Module):
+    def __init__(self, hidden_size, num_layers, h0=None):
+        super().__init__()
+        input_size = 3
+        output_size = 3
+
+        self.rnn = torch.nn.RNN(input_size, hidden_size, num_layers)
+        if h0 == None:
+            self.h0 = torch.nn.Parameter(torch.randn(num_layers, hidden_size))
+        else:
+            assert h0.shape == (num_layers, hidden_size)
+            self.h0 = torch.nn.Parameter(h0)
+        self.linear = torch.nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        output, hn = self.rnn(x, self.h0)
+        output = self.linear(output)
+        return torch.sigmoid(output)
